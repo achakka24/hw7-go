1d3db2c8c3ae0d172fe68c2f31dd2b3f0191cfb5
Aditya Chakka <adityachakka@Adityas-MacBook-M4-Air.local>
2026-03-23 15:54:58 -0700

    Add table-driven tests for polynomial differentiation

--- main_test.go
+++ main_test.go
@@ -3,16 +3,30 @@ package main
 import "testing"
 
 func TestDifferentiate(t *testing.T) {
-	p, err := parse("x^3 + 2*x + 5")
-	if err != nil {
-		t.Fatal(err)
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"x", "1"},
+		{"5", "0"},
+		{"x^3 + 2*x + 5", "3*x^2 + 2"},
+		{"4*x^2 - 3*x + 7", "8*x - 3"},
+		{"9*x^5", "45*x^4"},
+		{"-x^2 + x - 1", "-2*x + 1"},
+		{"x^2 - x^2 + 5", "0"},
+		{"3*x^4 + 2*x^3 - x + 8", "12*x^3 + 6*x^2 - 1"},
 	}
 
-	got := format(simplify(differentiate(p)))
-	want := "3*x^2 + 2"
+	for _, tc := range tests {
+		p, err := parse(tc.input)
+		if err != nil {
+			t.Fatalf("parse(%q) failed: %v", tc.input, err)
+		}
 
-	if got != want {
-		t.Fatalf("got %q want %q", got, want)
+		got := format(simplify(differentiate(p)))
+		if got != tc.want {
+			t.Fatalf("input %q: got %q want %q", tc.input, got, tc.want)
+		}
 	}
 }
 
